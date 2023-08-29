bdfbb53333dc3e6d258079a04aa0f359da03e175
Senshi881 <caiogamer881@gmail.com>
2023-08-29 13:19:15 -0300

    AdiçãoAtualização de novos arquivos

--- ativ5.c
+++ ativ5.c
@@ -7,40 +7,44 @@ int main() {
   despesas1 = 0.99;
   despesas2 = 0.1;
 
-
-
-  printf("Me informe o valor de sua mercadoria para calcularmos o frete: ");
+  printf("Me informe o valor de sua mercadoria: ");
   scanf("%f", &custo);
-  while (getchar() != '\n');
+  while (getchar() != '\n')
+    ;
 
-  printf("Agora nos informe a sua distancia em KMs em comparacao a loja ate a sua casa: ");
+  printf("Agora nos informe a sua distancia que a mercadoria vai percorrer em "
+         "KMs: ");
   scanf("%f", &distancia);
-  while (getchar() != '\n');
+  while (getchar() != '\n')
+    ;
 
   frete = distancia * despesas1 * despesas2 + custo;
 
-
   printf("Informe agora o valor da venda oferecida: ");
   scanf("%f", &venda);
-  while (getchar() != '\n');
+  while (getchar() != '\n')
+    ;
 
   float lucro, pFrete;
 
   lucro = venda - frete;
   float lucroP = fabs(lucro);
-  if(lucro > 0){
+  if (lucro > 0) {
     pFrete = (lucro * 100) / frete;
   } else {
     pFrete = (lucroP * 100) / frete;
   }
   float pFreteP = fabs(pFrete);
 
-
   printf("\nO valor da sua mercadoria junto com o frete e de %.2fR$;", frete);
-  if (lucro > 0){
-    printf("\nA mercadoria recebeu um lucro de %.0f%%, ou seja, um aumento de %.2fR$;", pFrete, lucro);
+  if (lucro > 0) {
+    printf("\nA mercadoria recebeu um lucro de %.0f%%, ou seja, um aumento de "
+           "%.2fR$;",
+           pFrete, lucro);
   } else {
-    printf("\nA mercadoria recebeu um desconto de %.0f%%, ou seja, uma baixa de %.2fR$;", pFreteP, lucroP);
+    printf("\nA mercadoria recebeu um desconto de %.0f%%, ou seja, uma baixa "
+           "de %.2fR$;",
+           pFreteP, lucroP);
   }
   return 0;
 }
--- ativ8.c
+++ ativ8.c
@@ -3,28 +3,35 @@
 
 int main() {
 
-  float salarioA, salarioN;
+  float salarioA, salarioN, diferenca;
   int reajuste;
-
 
   printf("Quanto voce recebe por mes (salario)? ");
   scanf("%f", &salarioA);
-  while (getchar() != '\n');
+  while (getchar() != '\n')
+    ;
 
-  printf("Quantos %% sera seu reajuste (caso o reajuste seja negativo, me informe o percentual em negativo)?: ");
+  printf("Quantos %% sera seu reajuste (caso o reajuste seja negativo, me "
+         "informe o percentual em negativo)?: ");
   scanf("%d", &reajuste);
-  while (getchar() != '\n');
+  while (getchar() != '\n')
+    ;
 
   int reajusteP = fabs(reajuste);
 
-  if(reajuste > 0){
+  if (reajuste > 0) {
     salarioN = (salarioA * reajuste) / 100 + salarioA;
   } else {
     salarioN = salarioA - (salarioA * reajusteP) / 100;
   }
 
-  printf("Seu salario atual e de %.2fR$;", salarioN);
-
+  printf("Seu salario atual e de R$%.2f;", salarioN);
+  if (reajuste < 0) {
+    diferenca = salarioA - salarioN;
+  } else {
+    diferenca = salarioN - salarioA;
+  }
+  printf("\nObteve uma diferenca de R$%.2f", diferenca);
 
   return 0;
 }
